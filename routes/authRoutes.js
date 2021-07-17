4a394a2c10fa46e5040aa167bb9ce3e3ccb24927
cse-mkamble <cse.mkamble@gmail.com>
2021-07-17 11:24:49 +0530

    make it better

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const authController = require('../controllers/authController')
+const authMiddleware = require('../middleware/authMiddleware')
+
+router.post('/register', authController.register)
+router.post('/activation', authController.activateEmail)
+router.post('/login', authController.login)
+router.post('/forgot', authController.forgotPassword)
+router.post('/reset', authMiddleware, authController.resetPassword)
+router.post('/logout', authController.logout)
+router.post('/refresh_token', authController.generateAccessToken)
+
+
+module.exports = router
